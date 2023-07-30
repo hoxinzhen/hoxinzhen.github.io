29fdd9ceb4f8c64f7ac163023541683d498d9280
hoxinzhen <hoxinzhen@gmail.com>
2023-07-30 20:43:25 +0800

    does it work now ;(

--- script.js
+++ script.js
@@ -34,10 +34,3 @@ for (i = 0; i < collapse.length; i++) {
   });
 }
 
-window.addEventListener("load",function() {
-	// Set a timeout...
-	setTimeout(function(){
-		// Hide the address bar!
-		window.scrollTo(0, 1);
-	}, 0);
-});
